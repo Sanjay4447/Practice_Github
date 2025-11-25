fc4b86bf9b30a54db10505e6fa49e46e66cd7805
Sanjay <sanjaygoud1729@gmail.com>
2025-11-25 15:28:07 +0530

    2nd commit

--- dict.py
+++ dict.py
@@ -1,5 +1,6 @@
 dict_1 = dict(name = "sanjay",age = 23,country = "India")
 dict_2 = dict(name = "sher",age = 25,country = "India")
 dict_2["age"] = 26
+dict_1["country"] = "Delhi"
 print(dict_1)
 print(dict_2)
